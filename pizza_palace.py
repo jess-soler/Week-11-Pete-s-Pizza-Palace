3fd05d040e2df934e2ea4a3a80b235e433deae9a
Jessica Soler <solerj@wncc.edu>
2024-03-28 19:34:13 -0600

    created utils and pizza_palace
    calling title function, not working, push origin and reopen to see if it works

---
+++ pizza_palace.py
@@ -0,0 +1,43 @@
+"""
+    name: pizza_palace.py
+    author: Jessica Soler
+    created: 3/28/24
+    purpose: point of sale program
+"""
+
+#-------------------------------VARIABLES-------------------------------#
+# import utils.py
+import utils
+
+
+#TODO: use a main function
+def main():
+    #TODO: from utils.py use title()
+    print(utils.title("Welcome to Pete's Pizza Palace!"))
+    print("\nWelcome!S!")
+    
+#TODO: from utils.py use get_int()
+#TODO: global CONSTANT lists
+# PIZZA_TYPE # 5 pizzas of your chice (string)
+# PIZZA_SIZE # size and price are parallel lists (string)
+# PIZZA_PRICE # (int)
+#TODO: each menu choice will have a number as shown in the example run
+#TODO: use this number to get the index of the item in the corresponding list
+#***remember that a list starts with 0****
+#type of pizza
+#size of pizza
+#TODO: create and test the following functions
+#TODO:get_pizza_type()
+#use a for loop to display the info in the pizza type list
+#use utils.get_int() to get input from the user
+#return the index of the pizza type choice
+#TODO:get_pizza_size()
+#use a for loop to display the information in the pizza size list
+#use utils.get_int() to get input from user
+#return the index of the pizza size choice
+#TODO:display_purchase()
+#pass in the pizza type and pizza size
+#lookup the cost of the pizza in the price list
+#calculate the cost of the pizza
+#display the completed order
+
---
+++ utils.py
@@ -0,0 +1,85 @@
+"""
+    name: utils.py
+    author: Jessica Soler
+    created: 3/28/24
+    purpose: a utility module with commonly used functions
+"""
+
+#-------------------------------TITLE-------------------------------#
+def title(statement):
+    """ takes in string argument, return string with ascii decorations """
+    
+    # get the length of the statement
+    text_length = len(statement)
+    
+    # initialize the result string variable
+    title_string = ""
+    title_string = title_string + " +--" + "-" * text_length + "--+\n"
+    title_string = title_string + " |  " + statement + "  |\n"
+    title_string = title_string + "+--" + "-" * text_length + "--+"
+    
+    # return the contatenated title string
+    return title_string
+
+#-------------------------------GET INT-------------------------------#
+def get_int(prompt):
+    """ 
+        get an integer from the user with try catch
+        the prompt string parameter is used to ask the user
+        for the type of input needed
+    """
+    
+    # ask the user for an input based on the prompt: string parameter
+    num = input(prompt)
+    
+    # if the input is numeric, convert to int and return value
+    try:
+        return int(num)
+    
+    # if the input is not numeric,
+    # inform the user and ask for input agian
+    except ValueError:
+        print(f"You entered: {num}, which is not a whole number.")
+        print(f"Let's try that again.\n")
+        
+        # call function again, this is a recursive function call
+        return get_int(prompt)
+    
+#-------------------------------GET FLOAT-------------------------------#
+def get_float(prompt):
+    """
+        get a number from the user with try catch
+        the prompt string parameter is used to ask the user
+        for the type of input needed
+     """
+        
+    # ask the user for an input based on the what parameter
+    num = input(prompt)
+    
+    # if the input is numeric, convert to float and return value
+    try:
+        return float(num)
+    
+    # if the input is not numeric
+    # inform the user and ask for input again
+    except ValueError:
+        print(f"You entered: {num}, which is not a number.")
+        print(f"Let's try that again.\n")
+    
+        # call function again, this is a recursive function call
+        return get_float(prompt)
+    
+#-------------------------------MAIN-------------------------------#
+def main():
+    """ place code here to test the module """
+    print(title("Test the utils module"))
+    int_num = get_int("Please enter a whole number: ")
+    print(f"Your whole number is: {int_num}")
+    float_num = get_float("Please enter any number: ")
+    print(f"Your number is: {float_num}")
+    
+# if standalone program, call the main function
+# else use as a module
+if __name__ == "__main__":
+    main()
+        
